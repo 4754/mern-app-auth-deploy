743e5e201d81439a4f5990ed5d16cccfb0e03ddd
4754 <gaurav2k9ssk54@gmail.com>
2023-11-25 14:56:06 +0530

    removed static

--- server.js
+++ server.js
@@ -33,15 +33,16 @@ app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use('/api/users', userRoutes) 
 
-if(process.env.NODE_ENV === "Production") {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
+// if(process.env.NODE_ENV === "Production") {
+//     const __dirname = path.resolve();
+//     app.use(express.static(path.join(__dirname, 'frontend/dist')));
     
-    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'dist','index.html')))
-} else {
-    app.get('/',(req,res)=> res.send("server is running"));
-}
+//     app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'dist','index.html')))
+// } else {
+//     app.get('/',(req,res)=> res.send("server is running"));
+// }
 
+app.get('/',(req,res)=> res.send("server is running"));
 
 
 app.use(notFound);
